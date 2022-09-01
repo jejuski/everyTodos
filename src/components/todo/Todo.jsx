c2e2c3ccd40994f3e2bacf52a5df706089175922
moojecho <wpfh188@naver.com>
2022-09-01 14:58:27 +0900

    css 수정
    css 수정

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -9,9 +9,9 @@ function Todo({ todo }) {
   const dispatch = useDispatch();
 
   return (
-    <Card style={{ minHeight: "150px" }}>
+    <Card style={{ minHeight: "150px", width:"220px"}}>
       <div className="card-body">
-        <button className="btn-close" style={{ marginLeft: "14em" }} onClick={() => dispatch(__deleteTodoThunk(todo.id))} />
+        <button className="btn-close" style={{ marginLeft: "10em" }} onClick={() => dispatch(__deleteTodoThunk(todo.id))} />
         <Link to={`/${todo.id}`} key={todo.id} style={{ color: "black", textDecoration: "none" }}>
           <h4 className="card-title">{todo.title}</h4>
           <div className="card-text">{todo.writer}</div>
@@ -20,5 +20,4 @@ function Todo({ todo }) {
     </Card>
   );
 }
-
 export default Todo;
